d1a3529a4012049895b3299baee0a40a6aeca987
Gabriel <gabrielsribeiro9999@gmail.com>
2023-03-22 22:12:59 -0300

    Atual.

--- Test/test_pytest.py
+++ Test/test_pytest.py
@@ -1,5 +1,7 @@
+from codigo.jogo import brinks
 
-
-def test_meu_primeiro_teste():
-    assert 1 == 1
-
+def test_quando_brincadeira_receber_1_entao_deve_return_1():
+    entrada = 1
+    esperada = 1
+    resultado = brinks(1)
+    assert resultado == esperada
